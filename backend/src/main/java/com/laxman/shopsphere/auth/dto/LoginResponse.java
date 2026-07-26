dc2a817c0c76b9e662c83122ea3bfa1b61768e2d
laxmanik <laxmankuddemmi@gmail.com>
2026-07-26 18:21:08 +0530

    feat(auth): add login endpoint using AuthenticationManager

---
+++ backend/src/main/java/com/laxman/shopsphere/auth/dto/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.laxman.shopsphere.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String message;
+}
--- backend/src/main/java/com/laxman/shopsphere/security/config/SecurityConfig.java
+++ backend/src/main/java/com/laxman/shopsphere/security/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.laxman.shopsphere.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -30,5 +34,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 }
